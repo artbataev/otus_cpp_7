40fd96dfe61c52b665c22514aea0663095507a49
artbataev <artbataev@gmail.com>
2018-08-26 19:47:15 +0300

    Full working implementation (including logging to file)

--- bulk.cpp
+++ bulk.cpp
@@ -16,8 +16,15 @@ public:
 
 };
 
-void print_commands(const std::vector<std::string>& commands) {
-    Logger::get_logger().add_stream("stdout", std::cout);
+std::string get_log_file_name(long long int first_command_time) {
+    std::stringstream ss;
+    ss << "bulk" << first_command_time << ".log";
+    return ss.str();
+}
+
+void print_commands(const std::vector<std::string>& commands, long long int first_command_time) {
+    std::ofstream f(get_log_file_name(first_command_time));
+    Logger::get_logger().add_stream("file", f);
     for (size_t i = 0; i < commands.size(); i++) {
         if (i > 0) {
             Logger::get_logger() << " ";
@@ -25,10 +32,12 @@ void print_commands(const std::vector<std::string>& commands) {
         Logger::get_logger() << commands[i];
     }
     Logger::get_logger() << "\n";
+    Logger::get_logger().remove_stream("file");
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) throw std::logic_error("Execute with one argument - number of commands in bulk");
+
     size_t num_commands_in_bulk;
     num_commands_in_bulk = static_cast<size_t>(std::stoi(argv[1]));
     int num_brackets = 0;
@@ -40,6 +49,9 @@ int main(int argc, char *argv[]) {
 //    auto& source_stream = from_file;
     auto& source_stream = std::cin;
 
+    Logger::get_logger().add_stream("stdout", std::cout);
+    long long int command_time = 0;
+
     while (source_stream >> current_command) {
         if (current_command == "{") {
             num_brackets++;
@@ -48,19 +60,22 @@ int main(int argc, char *argv[]) {
 
             num_brackets--;
             if (num_brackets == 0) {
-                print_commands(current_bulk);
+                print_commands(current_bulk, command_time);
                 current_bulk.clear();
             }
         } else { // normal command
+            if (current_bulk.empty()) {
+                command_time = std::chrono::system_clock::now().time_since_epoch().count();
+            }
             current_bulk.emplace_back(current_command);
             if (num_brackets == 0 && current_bulk.size() == num_commands_in_bulk) {
-                print_commands(current_bulk);
+                print_commands(current_bulk, command_time);
                 current_bulk.clear();
             }
         }
     }
     if (!current_bulk.empty() && num_brackets == 0) {
-        print_commands(current_bulk);
+        print_commands(current_bulk, command_time);
         current_bulk.clear();
     }
 
