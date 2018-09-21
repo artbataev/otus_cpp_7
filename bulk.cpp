be6f15f24f125780e7e9325f0a38e1da58894f57
artbataev <artbataev@gmail.com>
2018-09-21 12:19:03 +0200

    Check input arguments, do not throw error, just print help message

--- bulk.cpp
+++ bulk.cpp
@@ -95,10 +95,32 @@ private:
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) throw std::logic_error("Execute with one argument - number of commands in bulk");
+    bool executed_correctly = true;
+    std::stringstream whats_wrong;
+    int num_commands_in_bulk = 0;
 
-    int num_commands_in_bulk;
-    num_commands_in_bulk = std::stoi(argv[1]);
+    if (argc != 2) {
+        executed_correctly = false;
+        whats_wrong << "Incorrect number of arguments";
+    } else {
+        try {
+            num_commands_in_bulk = std::stoi(argv[1]);
+            if (num_commands_in_bulk <= 0) {
+                whats_wrong << "Number of commands in bulk must be positive";
+                executed_correctly = false;
+            }
+        } catch (std::exception&) {
+            executed_correctly = false;
+            whats_wrong << "Number of commands in bulk must be a number";
+        }
+    }
+
+    if (!executed_correctly) {
+        std::cout << "Error: " << whats_wrong.str() << std::endl;
+        std::cout << "Execute with one argument - number of commands in bulk, e.g.:" << std::endl;
+        std::cout << argv[0] << " 2" << std::endl;
+        exit(0);
+    }
 
 //    std::fstream from_file("../data/test1.in.txt");
 //    auto& source_stream = from_file;
